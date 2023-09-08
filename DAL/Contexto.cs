72a2743e1e88b2a4e548660bbc20b130a456e966
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-09-07 20:25:25 -0400

    Añadiendo el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ClientesApp.Models;
+
+namespace ClientesApp.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options)
+            : base(options) { }
+        public DbSet<Cliente> Cliente { get; set; }
+    }
+}
