9c7d8756b557438a6fc84ec74dccc77363f2ef9f
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-09-07 20:25:40 -0400

    Añadiendo el Models

---
+++ Models/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientesApp.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string? Nombres { get; set; }
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string? Telefono { get; set; }
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string? Celular { get; set; }
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string? Rnc { get; set; }
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string? Direccion { get; set; }
+        
+    }
+}
